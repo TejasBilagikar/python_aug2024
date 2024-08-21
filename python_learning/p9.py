218892dfc4a2ea734b83b3a4007e5d723bca55cf
TejasBilagikar <tejasbilagikar777@gmail.com>
2024-08-21 14:36:53 +0530

    program p9 updated

--- python_learning/p9.py
+++ python_learning/p9.py
@@ -1,7 +1,7 @@
 #Program to print Math table for a number.
 
-print('Code for Math table of a number.')
+print('Program for Math table of a number.')
 num = int(input('Enter a number: '))
 print(f'Math table for {num}: ')
-for i in range(1,11):
-  print(f'{num} x {i:2d} = {num * i}')
+for i in range(1,21):
+  print(f'{num:02d} x {i:02d} = {num * i:03d}')
